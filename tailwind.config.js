576b97d6343966fc940cb946869c41b43648f5cb
SnO2WMaN <me@sno2wman.net>
2021-08-19 09:11:38 +0900

    updateTailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const tailwindColors = require('tailwindcss/colors');
+
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   purge: ['./src/**/*.jsx', './src/**/*.tsx'],
   darkMode: 'media',
   plugins: [],
   theme: {
+    extend: {
+      colors: {
+        gray: tailwindColors.blueGray,
+      },
+    },
     zIndex: {
       0: 0,
       1: 1,
